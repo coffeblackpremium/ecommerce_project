390c0276174352c25cd838d370b7f43dcf303a77
João Damaceno <joaodamaceno188@gmail.com>
2021-12-12 19:40:37 -0400

    Alteraçoes feitas na Navbar, Adicionado o Static Files, IMG do Cart

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    return render(request, 'partials/base.html')
+
+def cart(request):
+    return render(request, 'cart.html')
