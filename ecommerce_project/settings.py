356a84201fa675fd9dae19ba36d5b22fe6d8b42f
João Damaceno <joaodamaceno188@gmail.com>
2021-12-12 18:37:12 -0400

    Configurado o Banco de Dados para o MYSQL, criado pasta Templates

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce_project',
+        'USER': 'joao',
+        'PASSWORD': 'songao5678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
